b254086f61863c2303e46430b2cffd809d6100bd
tesya <tesyasakun@gmail.com>
2017-12-20 13:51:36 +0700

    change directory

--- app/src/main/java/activities/AboutActivity.java
+++ app/src/main/java/activities/AboutActivity.java
@@ -1,4 +1,4 @@
-package com.lulua.tesyant.householdmanager.activities;
+package activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lulua.tesyant.householdmanager.activities;
+package activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
