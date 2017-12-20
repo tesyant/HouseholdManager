16e63821c66dce12e74df8b9ce82ba4c31c9ee39
tesya <tesyasakun@gmail.com>
2017-12-20 12:38:04 +0700

    about activity

--- app/src/main/java/com/lulua/tesyant/householdmanager/AboutActivity.java
+++ app/src/main/java/com/lulua/tesyant/householdmanager/AboutActivity.java
@@ -1,14 +1,14 @@
 package com.lulua.tesyant.householdmanager;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_about);
+        setTitle(getString(R.string.about));
     }
 }
