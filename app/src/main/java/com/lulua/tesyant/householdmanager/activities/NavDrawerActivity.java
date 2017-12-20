1b60cb6d4520b166a5ca07c8d4f81bb4a6a8e42e
tesya <tesyasakun@gmail.com>
2017-12-20 12:40:35 +0700

    dir activities
    refactor directory "activiities"

--- app/src/main/java/com/lulua/tesyant/householdmanager/activities/NavDrawerActivity.java
+++ app/src/main/java/com/lulua/tesyant/householdmanager/activities/NavDrawerActivity.java
@@ -1,4 +1,4 @@
-package com.lulua.tesyant.householdmanager;
+package com.lulua.tesyant.householdmanager.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.lulua.tesyant.householdmanager.R;
 
 public class NavDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
