8210d80851b1021b59183368e444e7ac3a3396e1
tesya <tesyasakun@gmail.com>
2017-12-24 15:26:46 +0700

    fixedNeed model implement parcelable

--- app/src/main/java/com/lulua/tesyant/householdmanager/models/FixedNeeds.java
+++ app/src/main/java/com/lulua/tesyant/householdmanager/models/FixedNeeds.java
@@ -1,15 +1,36 @@
 package com.lulua.tesyant.householdmanager.models;
 
-public class FixedNeeds extends Needs {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class FixedNeeds extends Needs implements Parcelable {
 
     private String tanggalBayar;
+
     public FixedNeeds() {
         super();
     }
+
     public FixedNeeds(String nama, int jumlah, double harga, String tglBayar) {
         super(nama, jumlah, harga);
         this.tanggalBayar = tglBayar;
     }
+
+    protected FixedNeeds(Parcel in) {
+        tanggalBayar = in.readString();
+    }
+
+    public static final Creator<FixedNeeds> CREATOR = new Creator<FixedNeeds>() {
+        @Override
+        public FixedNeeds createFromParcel(Parcel in) {
+            return new FixedNeeds(in);
+        }
+
+        @Override
+        public FixedNeeds[] newArray(int size) {
+            return new FixedNeeds[size];
+        }
+    };
 
     public String getTanggalBayar() {
         return tanggalBayar;
@@ -17,5 +38,15 @@ public class FixedNeeds extends Needs {
 
     public void setTanggalBayar(String tanggalBayar) {
         this.tanggalBayar = tanggalBayar;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(tanggalBayar);
     }
 }
