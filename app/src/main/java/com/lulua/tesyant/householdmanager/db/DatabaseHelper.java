4cb77902b23d09ad7e96a15e86923ca0f32d4ff4
tesya <tesyasakun@gmail.com>
2017-12-24 21:15:11 +0700

    fixing dbHelper

--- app/src/main/java/com/lulua/tesyant/householdmanager/db/DatabaseHelper.java
+++ app/src/main/java/com/lulua/tesyant/householdmanager/db/DatabaseHelper.java
@@ -11,57 +11,52 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public static String DB_NAME = "dbKebutuhan";
+    public static String DB_NAME = "dbNeeds";
 
-    public static String TABLE_NAME_KEB_TETAP = "FixedNeedsActivity";
-    public static String TABLE_NAME_KEB_TDKTETAP = "UnfixedNeedsActivity";
+    private static final String SQL_CREATE_TABLE_FIXED_NEEDS = String.format (
+            "CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL)",
 
-    public static String FIELD_ID_BARANG_TETAP = "IdBarang";
-    public static String FIELD_NAMA_BARANG_TETAP = "NamaBarang";
-    public static String FIELD_JANGKA_TETAP = "Jangka";
-    public static String FIELD_TGLBELI_TETAP = "TglBeli";
+            DatabaseContract.FixedNeedsColumns._ID,
+            DatabaseContract.FixedNeedsColumns.FIXED_NAME,
+            DatabaseContract.FixedNeedsColumns.FIXED_QUANTITY,
+            DatabaseContract.FixedNeedsColumns.FIXED_PRICE,
+            DatabaseContract.FixedNeedsColumns.FIXED_DATE
+    );
 
-    public static String FIELD_ID_BARANG_TDKTETAP = "IdBarangTdkTetap";
-    public static String FIELD_NAMA_BARANG_TDKTETAP = "NamaBarangTdkTetap";
-    public static String FIELD_TGLBELI_TDKTETAP = "TglBeliTdkTetap";
+    private static final String SQL_CREATE_TABLE_UNFIXED_NEEDS = String.format (
+            "CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL,"
+                    + " %s TEXT NOT NULL)",
 
-    public static String FIELD_JUMLAH = "Jumlah";
-    public static String FIELD_HARGA = "Harga";
+            DatabaseContract.UnfixedNeedsColumns._ID,
+            DatabaseContract.UnfixedNeedsColumns.UNFIXED_NAME,
+            DatabaseContract.UnfixedNeedsColumns.UNFIXED_QUANTITY,
+            DatabaseContract.UnfixedNeedsColumns.UNFIXED_PRICE,
+            DatabaseContract.UnfixedNeedsColumns.UNFIXED_DATE
+    );
 
-
-    public static String CREATE_TABLE_BARANG_TETAP = "CREATE TABLE " + TABLE_NAME_KEB_TETAP + " ( "
-            + FIELD_ID_BARANG_TETAP + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + FIELD_NAMA_BARANG_TETAP + " TEXT, "
-            + FIELD_JANGKA_TETAP + " INT, "
-            + FIELD_TGLBELI_TETAP + " DAT, "
-            + FIELD_JUMLAH + " INT, "
-            + FIELD_HARGA + " DOUBLE"
-            + " );";
-
-    public static String CREATE_TABLE_BARANG_TDKTETAP = "CREATE TABLE " + TABLE_NAME_KEB_TDKTETAP + " ( "
-            + FIELD_ID_BARANG_TDKTETAP + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + FIELD_NAMA_BARANG_TDKTETAP + " TEXT, "
-            + FIELD_TGLBELI_TDKTETAP + " DATE"
-            + FIELD_JUMLAH + " INT, "
-            + FIELD_HARGA + " DOUBLE"
-            + " );";
-
-
-    public DatabaseHelper(Context context) {
+    public DatabaseHelper (Context context) {
         super(context, DB_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_BARANG_TETAP);
-        db.execSQL(CREATE_TABLE_BARANG_TDKTETAP);
+        db.execSQL(SQL_CREATE_TABLE_FIXED_NEEDS);
+        db.execSQL(SQL_CREATE_TABLE_UNFIXED_NEEDS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_KEB_TETAP);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_KEB_TDKTETAP);
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_FIXED_NEEDS);
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_UNFIXED_NEEDS);
         onCreate(db);
     }
 }
