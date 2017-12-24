4dc3cb11819b493c039e1bc0a2576020c3e6487f
tesya <tesyasakun@gmail.com>
2017-12-24 17:13:20 +0700

    deleting toolbar and floatingbutton

--- app/src/main/java/com/lulua/tesyant/householdmanager/activities/FixedNeedsActivity.java
+++ app/src/main/java/com/lulua/tesyant/householdmanager/activities/FixedNeedsActivity.java
@@ -1,31 +1,47 @@
 package com.lulua.tesyant.householdmanager.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.lulua.tesyant.householdmanager.R;
+import com.lulua.tesyant.householdmanager.db.FixedNeedsHelper;
+import com.lulua.tesyant.householdmanager.models.FixedNeeds;
+
+import java.util.ArrayList;
 
 public class FixedNeedsActivity extends AppCompatActivity {
+
+    private FixedNeedsHelper fixedNeedsHelper;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fixed_needs);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_title_keb);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        recyclerView = findViewById(R.id.btn_recyclerview);
+
+        fixedNeedsHelper = new FixedNeedsHelper(this);
+        fixedNeedsHelper.open();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<FixedNeeds> result = new ArrayList<>();
+        result = fixedNeedsHelper.getAllData();
+
+        final String[] results = new String[result.size()];
+
+        if (result.size() > 0) {
+            for (int i=0; i<result.size(); i++) {
+                results[i] = String.valueOf(result.get(i));
             }
-        });
-    }
+        }
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+    }
 }
